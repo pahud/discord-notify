8ae5b7aed63bc817711ece7409279177ac093f92
Pahud Hsieh <pahudnet@gmail.com>
2023-05-13 04:17:57 +0000

    minor

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,10 +7,19 @@ const project = new typescript.TypeScriptAppProject({
     '@actions/core',
     '@actions/github',
   ],
+  devDeps: ['@vercel/ncc'],
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+const githubBuild = project.addTask('github:build', {
+  description: 'Prebuild setup for Gitpod',
+});
+githubBuild.exec('npx ncc build lib/index.js --license licenses.txt');
+
+project.gitignore.include('dist');
+
 project.synth();
