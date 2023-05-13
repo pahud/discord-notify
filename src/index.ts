15d81f3a9e1be200e7d3e436141796a9e657696b
Pahud Hsieh <pahudnet@gmail.com>
2023-05-13 04:26:43 +0000

    m

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
+// import * as github from '@actions/github';
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput('time', time);
+  const webhook = core.getInput('webhook');
+  const message = core.getInput('message');
+  console.log(`webhook: ${webhook}!`);
+  console.log(`message: ${message}!`);
+  // const time = (new Date()).toTimeString();
+  // core.setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2);
+  // console.log(`The event payload: ${payload}`);
 } catch (error: any) {
   core.setFailed(error.message);
 }
